a9e78e23c87e6b78a66ece4071fa268bd8b67eec
Maggie Bao <baox@berkeley.edu>
2023-04-27 17:35:44 -0700

    test1_initializeBoard

--- test_ticTacToe.py
+++ test_ticTacToe.py
@@ -1,10 +1,13 @@
-from ticTacToe import printGameBoard
-
+import pytest
+from ticTacToe import *
+from io import StringIO
+from unittest.mock import patch
+import builtins
 
 def test_InitialGameBoardshouldBePrintedCorrectly(capfd):
-    inital_gameBoard = gameBoard = [[1,2,3], [4,5,6], [7,8,9]]
-    printGameBoard(inital_gameBoard)
+    inital_gameBoard = [[1,2,3], [4,5,6], [7,8,9]]
+    print_game_board(inital_gameBoard)
     out, err = capfd.readouterr()
     correctBoard = """\n+---+---+---+\n| 1 | 2 | 3 |\n+---+---+---+\n| 4 | 5 | 6 |\n+---+---+---+\n| 7 | 8 | 9 |\n+---+---+---+\n"""
     assert out == correctBoard
-    
+
--- ticTacToe.py
+++ ticTacToe.py
@@ -1,17 +1,21 @@
 import random
 
-possibleNumbers = [1,2,3,4,5,6,7,8,9]
-gameBoard = [[1,2,3], [4,5,6], [7,8,9]]
+POSSIBLE_NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 ROWS = 3
 COLS = 3
 
-def printGameBoard(gameBoard):
-  for x in range(ROWS):
+def print_game_board(game_board):
+    for x in range(ROWS):
+        print("\n+---+---+---+")
+        print("|", end="")
+        for y in range(COLS):
+            print("", game_board[x][y], end=" |")
     print("\n+---+---+---+")
-    print("|", end="")
-    for y in range(COLS):
-      print("", gameBoard[x][y], end=" |")
-  print("\n+---+---+---+")
+    
 
-  
-
+def mark_tile(game_board, tile_number, player_mark):
+    """Updates the board state with the player's tile choice."""
+    for row in game_board:
+        for i, tile in enumerate(row):
+            if tile == tile_number:
+                row[i] = player_mark
